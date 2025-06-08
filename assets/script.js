c0f306ca6f2128678a7b5277292c95043ed6d442
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-06-07 22:33:12 -0300

    JavaScript
    Alterando a lógica de exibição de mensagens, e exibição de informações

--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,93 @@
-const cidade = document.querySelector('#cidadeInput');
-const search = document.querySelector('#search');
-const key = 'fb37b853ff466458e939d137aaac94c6';
+const cidade = document.querySelector("#cidadeInput");
+const search = document.querySelector("#search");
+const key = "fb37b853ff466458e939d137aaac94c6";
 
-const temperaturaSpan = document.querySelector('#tmeperatura');
-const cidadeNome = document.querySelector('#cidadeNome');
-const climaP = document.querySelector('#clima');
-const img = document.querySelector('#icon');
-const container = document.querySelector('#infosContent');
-const erro = document.querySelector('#erro');
-const carregando = document.querySelector('#carregamento');
+const temperaturaSpan = document.querySelector("#tmeperatura");
+const cidadeNome = document.querySelector("#cidadeNome");
+const climaP = document.querySelector("#clima");
+const img = document.querySelector("#icon");
+const container = document.querySelector("#infosContent");
+const fahrenheit = document.querySelector("#fahrenheit");
+const celsius = document.querySelector("#celsius");
 
-document.addEventListener('keydown', function(event){
-    
-    if(event.key === 'Enter'){
-        if(cidade.value.trim() !== ''){
-            consulta();
-        }else{
-            erro.style.display = 'block';
-            erro.innerHTML = 'Digite algo';
-        }
-    }
-})
+const alerta = document.querySelector("#alerta");
 
-search.addEventListener('click', () => {
-    if(cidade.value.trim() == ''){
-        erro.style.display = 'block';
-        erro.textContent = 'Digite algo';
-    }else{
-        consulta();
-    }
+// console.log(alerta.textContent);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") consulta();
 });
 
-function consulta(){
-    if(erro.style.display === 'block'){
-        erro.style.display = 'none'; 
-        erro.innerHTML = 'Não encontramos sua cidade';
-    }
+search.addEventListener("click", () => {
+  consulta();
+});
 
-    carregando.style.display = 'block';
-    fahrenheit.style.display = 'none';
-    celsius.style.display = 'none';
+function consulta() {
+  modfiAlerta(true, "Procurando...");
+  modfiContainer(false);
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade.value.trim()}&appid=${key}&units=metric&lang=pt`)
-    .then(response => response.json())
-    .then(data => {
-        carregando.style.display = 'none';
-        fahrenheit.style.display = 'none';
-        celsius.style.display = 'none';
-        if(data.name){
-            const name = data.name 
-            const clima = data.weather[0].description;
-            let temperatura = data.main.temp;
-            const icon = data.weather[0].icon;
-            const pais = data.sys.country;
+  fahrenheit.style.display = "none";
+  celsius.style.display = "none";
 
-            temperatura = temperatura.toFixed(2).replace(".", ",");
+  if (cidade.value.trim() === "") {
+    modfiAlerta(true, "Digite o nome de uma cidade");
+    return;
+  }
 
-            cidadeNome.textContent = name+", "+pais;
-            temperaturaSpan.textContent = temperatura  + "C°";
-            climaP.textContent = clima;
-            img.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-            container.style.display = 'block';
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade.value.trim()}&appid=${key}&units=metric&lang=pt`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      fahrenheit.style.display = "none";
+      celsius.style.display = "none";
 
-            const fahrenheit = document.querySelector('#fahrenheit');
-            const celsius  = document.querySelector('#celsius');
+      modfiAlerta();
 
-            fahrenheit.addEventListener('click', () => {
-                const newTemperatura = Number(temperatura.replace(',','.')) * 1.8 + 32;
-                temperaturaSpan.textContent = newTemperatura.toFixed(2).replace(".", ",") + "F°";
-            })
+      if (data.name) {
+        const name = data.name;
+        const clima = data.weather[0].description;
+        let temperatura = data.main.temp;
+        const icon = data.weather[0].icon;
+        const pais = data.sys.country;
 
-            fahrenheit.style.display = 'block';
+        temperatura = temperatura.toFixed(2).replace(".", ",");
 
-            celsius.addEventListener('click', () => {
-                temperaturaSpan.textContent = temperatura + "C°";
-            })
+        cidadeNome.textContent = name + ", " + pais;
+        temperaturaSpan.textContent = temperatura + "C°";
+        climaP.textContent = clima;
+        img.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-            celsius.style.display = 'block';
-            
-        }else{
-            erro.style.display = 'block';
-            erro.innerHTML = 'Não encontramos sua cidade';
+        modfiContainer(true);
 
-            if(container.style.display === 'block'){
-                container.style.display = 'none'; 
-            }
-        }
+        fahrenheit.addEventListener("click", () => {
+          const newTemperatura =
+            Number(temperatura.replace(",", ".")) * 1.8 + 32;
+          temperaturaSpan.textContent =
+            newTemperatura.toFixed(2).replace(".", ",") + "F°";
+        });
+
+        fahrenheit.style.display = "block";
+
+        celsius.addEventListener("click", () => {
+          temperaturaSpan.textContent = temperatura + "C°";
+        });
+
+        celsius.style.display = "block";
+      } else {
+        modfiAlerta(true, "Não encontramos sua cidade");
+        modfiContainer();
+      }
     })
-    .catch(error => console.error('erro', error));
+    .catch((error) => console.error("erro", error));
+}
+
+function modfiAlerta(visible, text) {
+  alerta.style.display = visible ? "block" : "none";
+  const strong = alerta.children;
+  strong[0].textContent = text;
+}
+
+function modfiContainer(visible) {
+  container.style.display = visible ? "block" : "none";
 }
